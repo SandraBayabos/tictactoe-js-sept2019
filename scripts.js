fab1785aea37470c33b62e3d62f969b1dd08040f
Sandra Bayabos <sandra.bayabos@gmail.com>
2019-10-04 12:33:20 +0800

    added checkWin function

--- scripts.js
+++ scripts.js
@@ -1,6 +1,10 @@
 // NUMBER OF TURNS
 let turn = 0;
 
+// CURRENT PLAYER
+
+let currentPlayer = "X";
+
 // WINNING COMBOS
 let winningCombos = [
   [0, 1, 2],
@@ -23,13 +27,34 @@ function nextMove(square) {
     } else {
       square.innerHTML = "X";
     }
+    checkWin();
   } else {
+    checkWin();
     alert("That spot's taken, dum dum.");
   }
 }
 
+// BOARD
+let board = document.getElementsByClassName("grid-item");
+console.log(board);
+
 // CHECK WINNER
-function checkWin() {}
+function checkWin() {
+  let won = false;
+  for (let i = 0; i < winningCombos.length; i++) {
+    let winningState = winningCombos[i]
+      .map(index => {
+        console.log(board[index].innerHTML);
+        return board[index].innerHTML;
+      })
+      .join("");
+    if (winningState === "XXX" || winningState === "OOO") {
+      won = true;
+      alert("Congrats. You won");
+    }
+  }
+  return won;
+}
 
 // // START GAME
 
